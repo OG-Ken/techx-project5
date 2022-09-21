c1be33ebf07ae5e8b1852cc4d4d9eb15b38a318d
Mathew <624949@bah.com>
2022-09-20 22:57:12 -0400

    Working on UI Appearance of Login Screen

--- reactApp/savewaste/src/App.js
+++ reactApp/savewaste/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./App.css";
-import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   return (
     <div>
-      <Signup />
+      <Login />
     </div>
   );
 }
